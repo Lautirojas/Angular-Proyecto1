12d135d1520281c4c68081e925cf21841e124dd8
Lautaro <lautarorojas572@gmail.com>
2022-11-18 20:24:45 -0300

    añadiendo componentes angular + tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// COMPONENTS
 import { AppComponent } from './app.component';
+import { MyNavbarComponent } from './components/my-navbar/my-navbar.component';
+import { MyToolbarComponent } from './components/my-toolbar/my-toolbar.component';
+import { MyContentComponent } from './components/my-content/my-content.component';
+import { MyFooterComponent } from './components/my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyToolbarComponent,
+    MyNavbarComponent,
+    MyContentComponent,
+    MyFooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
